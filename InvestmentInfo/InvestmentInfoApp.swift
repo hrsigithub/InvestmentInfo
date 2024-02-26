06567cffb419daf7c5778f932da00fa500411883
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-02-26 14:55:12 +0900

    Initial Commit

---
+++ InvestmentInfo/InvestmentInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentInfoApp.swift
+//  InvestmentInfo
+//
+//  Created by Hiroshi.Nakai on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
